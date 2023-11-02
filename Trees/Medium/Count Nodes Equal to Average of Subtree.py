da574a4483e77afa99160e1f5ad3b9efdf069396
nabilah-afrin <118561482+nabilah-afrin@users.noreply.github.com>
2023-11-02 19:11:42 +0600

    Create Count Nodes Equal to Average of Subtree.py

---
+++ Trees/Medium/Count Nodes Equal to Average of Subtree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# leetcode daily challenge
+# 02/11/2023
+
+class Solution:
+    def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
+        counter = 0
+
+        def post_order(root):
+            #global counter
+            nonlocal counter #unboundlocalerror
+            if root is None:
+                return (0, 0)  # sum and count
+
+            # return sum and count for left and right child
+            left_sum, left_count = post_order(root.left)
+            right_sum, right_count = post_order(root.right)
+
+            # find the total sum of the current node
+            node_sum = left_sum + right_sum + root.val
+            node_count = left_count + right_count + 1
+
+            avg = node_sum // node_count  
+
+            if avg == root.val:
+                counter += 1
+            return node_sum, node_count
+        
+        post_order(root)
+        return counter #return count after calling the function
