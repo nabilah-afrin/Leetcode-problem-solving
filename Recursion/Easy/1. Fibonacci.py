1176bf1b685ca450d47d4c4302fe3ca9a2a23a06
nabilah-afrin <118561482+nabilah-afrin@users.noreply.github.com>
2023-11-20 21:18:35 +0600

    Update 1. Fibonacci.py

--- Recursion/Easy/1. Fibonacci.py
+++ Recursion/Easy/1. Fibonacci.py
@@ -5,6 +5,13 @@ def fibonacci(n):
     while a<n:
         series.append(a)
         a, b = b, a+b
-    print(series)
+    print(series) #will create from 0,1,1
 
 fibonacci(4)
+
+"""in order to buikd from 1,1,2 
+a,b = 1,1
+for i in range(n):
+a,b = b, a+b
+return b"""
+while n>1: return fib(n-1) + fib(n-2)
